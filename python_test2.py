40e29384bf56fe1354490bb9e275800322cf45e9
M.A <infoarcsc27@gmail.com>
2020-10-31 15:41:22 +0900

    数値終了

---
+++ python_test2.py
@@ -0,0 +1,22 @@
+##2.1 四則演算
+test_integer2 = 100
+print(test_integer2 + 10)
+print(test_integer2 - 10)
+print(test_integer2 * 10)
+print(test_integer2 / 10)
+
+##2.2 数値への変換
+test_str2 = "100"
+print(int(test_str2)+100)
+
+##2.3 浮動少数点数
+test_str2 = "100.5"
+print(float(test_str2) + 100)
+test_float2 = .5
+print(test_float2)
+
+##2.4　複素数
+test_complex = 100 + 5j
+print(test_complex)
+print(test_complex.real)
+print(test_complex.imag)

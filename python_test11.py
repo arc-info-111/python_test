311a67a961f465b03ca20aa5f3dfa6170d63bd5f
M.A <infoarcsc27@gmail.com>
2020-11-07 16:48:47 +0900

    基礎編ファイルに移動

--- python_test11.py
+++
@@ -1,9 +0,0 @@
-# 11 rangeとxrange (breakとcontinueは省略)
-for i in range(10):
-    print(i)
-
-for i in range(2,9):
-    print(i)
-
-for i in range(-2,10):
-    print(i)
--- test_argv.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-args = sys.argv
-
-print(args)
-print("実行ファイル名" + args[0])
-print("第一引数" + args[1])
-print("第二引数" + args[2])
-print("第三引数" + args[3])
